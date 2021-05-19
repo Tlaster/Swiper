c30a677d1c7bc1468b6c9e97f6b98e411218ddcd
Tlaster <tlaster@outlook.com>
2021-05-19 12:00:56 +0800

    upgrade to compose beta07

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -54,7 +54,7 @@ dependencies {
     implementation("androidx.compose.material:material:${rootProject.extra["compose_version"]}")
     implementation("androidx.compose.ui:ui-tooling:${rootProject.extra["compose_version"]}")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.0-alpha01")
-    implementation("androidx.activity:activity-compose:1.3.0-alpha07")
+    implementation("androidx.activity:activity-compose:1.3.0-alpha08")
     testImplementation("junit:junit:4.+")
     androidTestImplementation("androidx.test.ext:junit:1.1.2")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
