5a1c40d5f4c9c54ee01720f86389f86f4b5c90c3
Tlaster <tlaster@outlook.com>
2021-07-02 11:59:05 +0800

    upgrade to compose rc01

--- swiper/build.gradle.kts
+++ swiper/build.gradle.kts
@@ -40,7 +40,7 @@ afterEvaluate {
             create<MavenPublication>("release") {
                 groupId = "moe.tlaster"
                 artifactId = "swiper"
-                version = "0.5.0"
+                version = "0.6.0"
 
                 from(components["release"])
             }
